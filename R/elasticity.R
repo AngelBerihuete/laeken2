f8f9a0ddbb489a2676e454584be731bf17b95f88
A. Berihuete <angel.berihuete@uca.es>
2014-10-29 17:03:30 +0100

    up-date elasticity

--- R/elasticity.R
+++ R/elasticity.R
@@ -8,12 +8,11 @@ elasticity <- function(dataset, samp = 10){
   dataset <- subset(dataset, ipuc>0)
   clg <- glc(dataset, samp)
   clg <- clg[-1,]
-  clg$y.lorenz <- clg$y.lg/miuc(dataset)
   p <- clg$x.lg
-  Lp <- clg$y.lorenz
-  spl <- smooth.spline(p, Lp)
-  Lp.deriv <- predict(spl, p , deriv=1)
-  celas <- (p*Lp.deriv$y)/Lp
+  lp <- clg$y.lg/miuc(dataset)
+  spl <- splinefun(p, lp)
+  dlp <- spl(p, deriv = 1)
+  celas <- (p*dlp)/lp
   results <- data.frame(x.elas = p, y.elas = celas)
   return(results)
 }
